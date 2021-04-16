db55c5d6ecd7bb30f66e31075e2989655bec0241
Peter Rigo <boys4849@gmail.com>
2021-04-16 13:15:57 +0200

    d

--- models/Holiday.php
+++ models/Holiday.php
@@ -18,5 +18,29 @@ class Holiday
     {
         $this->conn = $db;
     }
+    // read products
+    function read(){
+
+        // select all query
+        $query = "SELECT
+                d.day,d.month, p.id, p.day_id, p.country_id, p.name
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    days d
+                        ON p.day_id = d.id
+                        LEFT JOIN
+                    countries c
+                        ON p.country_id = c.id
+                        GROUP BY p.id";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
 }
 ?>
--- service/read.php
+++ service/read.php
@@ -1,15 +1,51 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-include_once '../config/database.php';
-include_once '../models/holiday.php';
+include_once '../config/Database.php';
+include_once '../models/Holiday.php';
 
 // instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
 
 // initialize object
+$product = new Holiday($db);
 
+// query products
+$stmt = $product->read();
+$num = $stmt->rowCount();
 
-// read products will be here
+// check if more than 0 record found
+if($num>0){
 
+    // products array
+    $products_arr=array();
+    $products_arr["records"]=array();
+
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+
+        $product_item=array(
+            "id" => $id,
+            "name" => $name,
+            "day_id" => $day_id,
+            "country_id" => $country_id,
+        );
+
+        array_push($products_arr["records"], $product_item);
+    }
+
+    // set response code - 200 OK
+    http_response_code(200);
+
+    // show products data in json format
+    echo json_encode($products_arr);
+}
+
+// no products found will be here
